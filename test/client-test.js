9703a24b8c21dbd9642431aaf60dcdfb625a5c51
Daniel hernandez <danielhernandez16@hotmail.com>
2016-11-13 19:48:01 -0500

    Create test client basic

---
+++ test/client-test.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const test = require('ava')
+// requiriendo archivo index en la raiz
+const mygram = require('../')
+// const fixtures = require('./fixtures')
+
+test('client', t => {
+  const client = mygram.createClient()
+
+  t.is(typeof client.getPicture, 'function')
+  t.is(typeof client.savePicture, 'function')
+  t.is(typeof client.likePicture, 'function')
+  t.is(typeof client.listPictures, 'function')
+  t.is(typeof client.listPicturesByTag, 'function')
+  t.is(typeof client.saveUser, 'function')
+  t.is(typeof client.getUser, 'function')
+  t.is(typeof client.auth, 'function')
+})
