b5ada0e33b8ce775abb82ecc2bd8efecafd6357c
Daniel hernandez <danielhernandez16@hotmail.com>
2016-11-14 00:01:57 -0500

    Test listPictures

--- test/client-test.js
+++ test/client-test.js
@@ -100,3 +100,17 @@ test('likePicture', async t => {
 
   t.deepEqual(image, result)
 })
+
+test('listPicture', async t => {
+  const client = t.context.client
+
+  let images = fixtures.getImages(3)
+
+  nock(options.endpoints.pictures)
+    .get('/list')
+    .reply(200, images)
+
+  let result = await client.listPictures()
+
+  t.deepEqual(images, result)
+})
